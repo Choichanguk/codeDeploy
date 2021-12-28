7e683bbed2c3145c51fa1bd42d5a6ec215225816
Choichanguk <ckddnr1213@gmail.com>
2021-12-28 04:27:42 +0000

    deploy test commit first

---
+++ app.js
@@ -0,0 +1,15 @@
+import http from 'http';
+import express from 'express';
+
+let app = express();
+
+app.use('/', (req, res) => {
+    res.send("test server");
+})
+
+let httpServer = http.createServer(app);
+
+httpServer.listen(8443, () => {
+    console.log('Kurento Group Call started');
+    console.log('listening on *:', 8443);
+});
