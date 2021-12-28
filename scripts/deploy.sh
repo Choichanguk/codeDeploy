501ae971d82dc7196115cddd861788957e1734ec
Choichanguk <ckddnr1213@gmail.com>
2021-12-28 08:05:31 +0000

    third

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-echo "before create directory"
-mkdir /home/ubuntu/test2
-echo "after create directory"
+curl -sL https://deb.nodesource.com/setup_14.x| sudo -E bash -
+sudo apt-get install -y nodejs
+# shellcheck disable=SC2164
+cd /home/ubuntu/test
+npm start
